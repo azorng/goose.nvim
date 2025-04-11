546a6c97a557697943b7cfd72b2117b52d42f625
azor <azorng@gmail.com>
2025-04-12 01:55:21 +0200

    add session bar

--- lua/goose/ui/highlight.lua
+++ lua/goose/ui/highlight.lua
@@ -3,6 +3,11 @@ local M = {}
 function M.setup()
   vim.api.nvim_set_hl(0, 'GooseBorder', { fg = '#616161' })
   vim.api.nvim_set_hl(0, 'GooseBackground', {})
+  vim.api.nvim_set_hl(0, 'GooseSessionDescription', {
+    -- bg = '#212121',
+    fg = '#757575',
+    bold = false,
+  })
 end
 
 return M
