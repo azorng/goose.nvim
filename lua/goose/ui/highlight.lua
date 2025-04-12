779e180b1b433bd5cc497b1455d8fcd957ca188b
azor <azorng@gmail.com>
2025-04-12 02:17:52 +0200

    comment style

--- lua/goose/ui/highlight.lua
+++ lua/goose/ui/highlight.lua
@@ -4,9 +4,7 @@ function M.setup()
   vim.api.nvim_set_hl(0, 'GooseBorder', { fg = '#616161' })
   vim.api.nvim_set_hl(0, 'GooseBackground', {})
   vim.api.nvim_set_hl(0, 'GooseSessionDescription', {
-    -- bg = '#212121',
-    fg = '#757575',
-    bold = false,
+    link = 'Comment',
   })
 end
 
