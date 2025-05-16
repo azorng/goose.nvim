1b3158f2fecaafaac1f94507820959621a641bdb
azor <azorng@gmail.com>
2025-05-16 23:56:25 +0300

    reduce keymap maintenance

--- lua/goose/keymap.lua
+++ lua/goose/keymap.lua
@@ -2,39 +2,19 @@ local api = require("goose.api")
 
 local M = {}
 
+-- Binds a keymap config with its api fn
+-- Name of api fn & keymap global config should always be the same
 function M.setup(keymap)
   local cmds = api.commands
   local global = keymap.global
 
-  local all_mappings = {
-    'open_input',
-    'open_input_new_session',
-    'open_output',
-    'close',
-    'toggle_fullscreen',
-    'select_session',
-    'toggle',
-    'toggle_focus',
-    'configure_provider',
-    'goose_mode_chat',
-    'goose_mode_auto',
-    'diff_open',
-    'diff_next',
-    'diff_prev',
-    'diff_close',
-    'diff_revert_all',
-    'diff_revert_this',
-  }
-
-  for _, key in ipairs(all_mappings) do
-    if global[key] then
-      vim.keymap.set(
-        { 'n', 'v' },
-        global[key],
-        function() api[key]() end,
-        { silent = false, desc = cmds[key] and cmds[key].desc }
-      )
-    end
+  for key, mapping in pairs(global) do
+    vim.keymap.set(
+      { 'n', 'v' },
+      mapping,
+      function() api[key]() end,
+      { silent = false, desc = cmds[key] and cmds[key].desc }
+    )
   end
 end
 
